3d0e4d9f02e80a4c09a3896e22115daf96797186
chetandchavan <101565537+chetandchavan@users.noreply.github.com>
2022-03-16 14:36:01 +0530

    Add files via upload

---
+++ Gretest.java
@@ -0,0 +1,20 @@
+// find the gretest no from 3 numbers
+// input = 10,20,30.
+class Gretest {
+      public static void main(String arg []){
+	  int a=40, b=50,int c=80;
+	     if (a > b)
+		 {
+		     if (a > c )
+			 System.out.println(a);
+			 else
+			 System.out.println(c);
+		 }
+		 else
+		 if (b > c )
+		 System.out.println(b);
+		 else
+		 System.out.println(c);
+		
+		}
+}
